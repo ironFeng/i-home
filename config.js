f1804eee5d11035b75c376d5ceac3740d23f9735
ironFeng <phvos@163.com>
2017-05-24 11:32:28 +0800

    initialize the project

---
+++ config.js
@@ -0,0 +1,4 @@
+var FILESAVEPLACE = 'upload'
+var TEMPFILESAVEPLACE = 'D:/code/nodejs/photoserver/public/images/'
+module.exports.FILESAVEPLACE = FILESAVEPLACE;
+module.exports.TEMPFILESAVEPLACE = TEMPFILESAVEPLACE;
